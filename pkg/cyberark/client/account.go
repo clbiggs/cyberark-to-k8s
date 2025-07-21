f11199f13a0269b0c3d5e68faef7a44a556c6d40
Christopher Biggs <christopher.l.biggs@gmail.com>
2025-07-20 21:04:33 -0400

    Added cyberark api models

---
+++ pkg/cyberark/client/account.go
@@ -0,0 +1,41 @@
+package client
+
+type Account struct {
+	CategoryModificationTime  int64             `json:"categoryModificationTime"`
+	ID                        string            `json:"id"`
+	Name                      string            `json:"name"`
+	Address                   string            `json:"address"`
+	PlatformID                string            `json:"platformId"`
+	SafeName                  string            `json:"safeName"`
+	UserName                  string            `json:"userName"`
+	SecretType                SecretType        `json:"secretType"`
+	PlatformAccountProperties map[string]string `json:"platformAccountProperties"`
+	SecretManagement          SecretManagement  `json:"secretManagement"`
+	CreatedTime               int64             `json:"createdTime"`
+}
+
+type SecretType string
+
+const (
+	SecretTypePassword SecretType = "password"
+	SecretTypeKey      SecretType = "key"
+)
+
+type SecretManagement struct {
+	AutomaticManagementEnabled bool                    `json:"automaticManagementEnabled"`
+	ManualManagementReason     string                  `json:"manualManagementReason"`
+	Status                     *SecretManagementStatus `json:"status,omitempty"`
+	LastModifiedDateTime       uint64                  `json:"lastModifiedDateTime"`
+	LastReconciledDateTime     *uint64                 `json:"lastReconciledDateTime"`
+	LastVerifiedDateTime       *uint64                 `json:"lastVerifiedDateTime"`
+}
+
+type SecretManagementStatus string
+
+const (
+	SecretManagementStatusInProcess SecretManagementStatus = "inProcess"
+	SecretManagementStatusSuccess   SecretManagementStatus = "success"
+	SecretManagementStatusFailure   SecretManagementStatus = "failure"
+)
+
+type Password string
